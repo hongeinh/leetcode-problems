b8b65b3a8548040833f51cd45f9a00b2a8340af5
anhnth <anh.nth3179@gmail.com>
2024-08-24 19:03:31 -0700

    greedy

---
+++ greedy/jump_game_ii.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/jump-game-ii/
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        near=far=jump = 0
+
+        while far < len(nums) - 1:
+            farthest = 0
+            for i in range(near, far + 1):
+                farthest = max(farthest, i + nums[i])
+
+            near = far + 1
+            far = farthest
+            jump += 1
+        return jump
+        
+sol = Solution()
+# nums = [1,2,1,1,1]
+nums = [0]
+print(sol.jump(nums))
