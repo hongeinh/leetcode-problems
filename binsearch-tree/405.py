8b7368316e85fd508c6a256fdaf8cbfdca81b81c
Hồng Einh <44611968+hongeinh@users.noreply.github.com>
2025-01-23 14:43:01 +0900

    binary search tree problems

---
+++ binsearch-tree/405.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/delete-node-in-a-bst/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
+        else:
+            # find successor
+            successor = root.right
+            while successor and successor.left:
+                successor = successor.left
+            root.val = successor.val
+            root.right =  self.deleteNode(root.right, successor.val)
+        return root
