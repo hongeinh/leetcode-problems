c625b6b99cde87c7b7715affdc55a97e7ea74114
Hong Anh <44611968+hongeinh@users.noreply.github.com>
2025-04-09 20:42:25 -0700

    daily

---
+++ arrays/3375.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        unique = set()
+        for num in nums:
+            if num < k:
+                return -1
+            elif num > k:
+                unique.add(num)
+
+        return len(unique)
